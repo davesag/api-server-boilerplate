a433fd1f35aecac3c3e73b4c88a485da5ef1756b
Dave Sag <davesag@gmail.com>
2019-05-04 08:42:21 +0530

    n/a moved test helpers and simplfied server tests

--- test/server/routes/v1/hello.test.js
+++ test/server/routes/v1/hello.test.js
@@ -1,6 +1,6 @@
 const { expect } = require('chai')
 const request = require('supertest')
-const serverCache = require('test/server/serverCache')
+const serverCache = require('test/utils/serverCache')
 const { BAD_REQUEST } = require('http-status-codes')
 
 describe('GET /api/v1/hello/:name', () => {
@@ -12,31 +12,21 @@ describe('GET /api/v1/hello/:name', () => {
 
   context('given a valid name', () => {
     const name = 'Dave Sag'
-    const expected = `hello ${name}`
-    it('returns the expected value with status code 200', done => {
+    const expected = `"hello ${name}"`
+
+    it('returns the expected value with status code 200', async () =>
       request(server)
         .get(`/api/v1/hello/${name}`)
-        .end((err, res) => {
-          expect(err).to.not.exist
-          expect(res.statusCode).to.equal(200)
-          expect(res.body).to.deep.equal(expected)
-          done()
-        })
-    })
+        .expect(200, expected))
   })
 
   context('given an invalid name', () => {
     const name = 'Dave 546'
-    const expected = { error: `${name} is not a valid name.` }
-    it('returns the expected error message and status code BAD_REQUEST', done => {
+    const expected = JSON.stringify({ error: `${name} is not a valid name.` })
+
+    it('returns the expected error message and status code BAD_REQUEST', async () =>
       request(server)
         .get(`/api/v1/hello/${name}`)
-        .end((err, res) => {
-          expect(err).to.not.exist
-          expect(res.statusCode).to.equal(BAD_REQUEST)
-          expect(res.body).to.deep.equal(expected)
-          done()
-        })
-    })
+        .expect(BAD_REQUEST, expected))
   })
 })
