424b775694c09418fbd54f981e1b14f643439565
Dave Sag <davesag@gmail.com>
2019-07-12 11:38:13 +0530

    #3 added example of async route

---
+++ test/server/routes/v1/snooze.test.js
@@ -0,0 +1,15 @@
+const request = require('supertest')
+const serverCache = require('test/utils/serverCache')
+
+describe('GET /api/v1/snooze', () => {
+  let server
+
+  before(() => {
+    server = serverCache.get()
+  })
+
+  it('returns status code 200', async () =>
+    request(server)
+      .get('/api/v1/snooze')
+      .expect(200))
+})
